12297187f5b01088b5472212bb766921988a3d43
ajcross023 <s1239598@monmouth.edu>
2023-08-01 22:10:26 -0400

    section 6 complete

--- NotesSect6/Operators.rb
+++ NotesSect6/Operators.rb
@@ -3,3 +3,8 @@ puts 2-1 #subtraction
 puts 2*2 #multiplication
 puts 4/2 #division
 puts 2**3 #exponents
+#do you remember pemdas? you better
+#Integer - 2, 5, 5765
+#Float - 1.2, 1.3, 6.5
+#Decimal - 2.34246734, 5.34523713
+#Don't forget that integers and floats matter when doing calcualtions!
